80b0f76bb0d4c97d82b769397d4978a5c943abeb
gsarwate <4589380+gsarwate@users.noreply.github.com>
2020-01-11 19:44:40 -0800

    create shapes and translate them

--- test/brick_test.exs
+++ test/brick_test.exs
@@ -2,6 +2,7 @@ defmodule BrickTest do
   use ExUnit.Case
 
   import Tetris.Brick
+  alias Tetris.Points
 
   test "Creates a new brick" do
     assert new_brick().name == :i
@@ -16,17 +17,37 @@ defmodule BrickTest do
   end
 
   test "should manipulate brick" do
-    actual = new_brick()
-    |> left()
-    |> right()
-    |> right()
-    |> down()
-    |> spin_90()
-    |> spin_90()
+    actual =
+      new_brick()
+      |> left()
+      |> right()
+      |> right()
+      |> down()
+      |> spin_90()
+      |> spin_90()
 
     assert actual.location == {41, 1}
     assert actual.rotation == 180
   end
 
-  def new_brick, do: new()
+  test "should return points for i shape" do
+    points = new_brick(name: :i) |> shape()
+
+    assert {2, 2} in points
+  end
+
+  test "should return points for o shape" do
+    points = new_brick(name: :o) |> shape()
+
+    assert {3, 3} in points
+  end
+
+  test "should translate a list of points" do
+    actual_points =
+      new_brick() |> shape() |> Points.translate({1, 1}) |> Points.translate({0, 1})
+
+    assert actual_points == [{3, 3}, {3, 4}, {3, 5}, {3, 6}]
+  end
+
+  def new_brick(attributes \\ []), do: new(attributes)
 end
